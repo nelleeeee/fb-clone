baf1574572a0b9c24639c93d7fe4c5725bf16808
nelleeeee <rudghksldl@gmail.com>
2020-09-18 01:39:41 +0900

    init, header

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCgqtxB-vOPvlYra7GcGowAXiFTZc8AeP8",
+  authDomain: "fb-clone-dd68e.firebaseapp.com",
+  databaseURL: "https://fb-clone-dd68e.firebaseio.com",
+  projectId: "fb-clone-dd68e",
+  storageBucket: "fb-clone-dd68e.appspot.com",
+  messagingSenderId: "948646646294",
+  appId: "1:948646646294:web:0332540e78948badfe9b37",
+  measurementId: "G-ZR3HRVCDDC",
+};
